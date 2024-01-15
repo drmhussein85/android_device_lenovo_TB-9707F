675ef6840e3946ddad7121413d235afdf4e77bbb
drmhussein85 <66475390+drmhussein85@users.noreply.github.com>
2024-01-15 10:16:57 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TB-9707F-user
-add_lunch_combo omni_TB-9707F-userdebug
-add_lunch_combo omni_TB-9707F-eng
+add_lunch_combo twrp_TB-9707F-user
+add_lunch_combo twrp_TB-9707F-userdebug
+add_lunch_combo twrp_TB-9707F-eng
